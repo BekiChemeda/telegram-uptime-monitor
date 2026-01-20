890ed788f637d404441c2e10242e1860ec849652
Beki <bekichemeda@gmail.com>
2026-01-20 09:34:37 +0300

    feat: Add main execution block to initialize database

--- app/database/init_db.py
+++ app/database/init_db.py
@@ -5,3 +5,6 @@ from models import Base
 async def init_db():
     async with engine.begin() as conn:
         conn.run_sync(Base.metadata.create_all)
+
+if __name__ == "__main__":
+    asyncio.run(init_db())
