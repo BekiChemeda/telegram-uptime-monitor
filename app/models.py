6d13a8f733c338262753ad31d10313369f154562
Beki <bekichemeda@gmail.com>
2026-01-20 09:45:10 +0300

    feat: Update User and Website models to use BigInteger for IDs and adjust default datetime format

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime, BigInteger
 from sqlalchemy.orm import declarative_base
 from datetime import datetime
 Base = declarative_base()
@@ -6,15 +6,15 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    telegramId = Column(Integer, unique=True, nullable=False)
+    telegramId = Column(BigInteger, unique=True, nullable=False)
     username = Column(String, unique=True)
-    joinedAt = Column(DateTime, nullable=False, default=datetime.now().isoformat())
+    joinedAt = Column(DateTime, nullable=False, default=datetime.now)
 
 
 class Website(Base):
     __tablename__ = "websites"
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    ownerId = Column(Integer, unique=True, nullable=False)
+    ownerId = Column(BigInteger, nullable=False)
     url = Column(String, unique=True, nullable=False)
-    createdAt = Column(DateTime, nullable=False, default=datetime.now().isoformat())
-    lastChecked = Column(DateTime, nullable=False, default=datetime.now().isoformat())
+    createdAt = Column(DateTime, nullable=False, default=datetime.now)
+    lastChecked = Column(DateTime, nullable=False, default=datetime.now)
